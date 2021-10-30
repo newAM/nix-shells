44db40bd3a74102b61655e077533593ba0d3fdbf
Alex Martens <alexmgit@protonmail.com>
2021-10-30 07:46:57 -0700

    move pins to a central directory

---
+++ pins/2021-10-29.nix
@@ -0,0 +1,8 @@
+import
+  (builtins.fetchTarball {
+    name = "nixpkgs-2021-10-16";
+    # nix-prefetch-url --unpack <url>
+    url = "https://github.com/nixos/nixpkgs/archive/b1c9da88495de94950463e31f584af0d42419123.tar.gz";
+    sha256 = "0mvxy99c2hrsh872p6cvvgbngskvgm5h42rgykn2va01x63ndzha";
+  })
+{ }
