3206193546bfd8da34b94afb5ee865ef16f16db2
Alex Martens <alex@thinglab.org>
2022-08-01 09:20:22 -0700

    flake.nix: do not create an instance of nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let pkgs = import nixpkgs { system = "x86_64-linux"; }; in {
+  outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
     packages.x86_64-linux = {
       form = pkgs.callPackage ./pkgs/form { };
     };
